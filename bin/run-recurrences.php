0810337f204ce17daaf43cc2432a8c492804b402
Pedro Leandro Gomes da Silva <pedroleandrogomesdasilva@outlook.com>
2026-09-25 10:18:10 -0300

    fix: usar require_once no script de recorrências para evitar redefinição de constantes

--- bin/run-recurrences.php
+++ bin/run-recurrences.php
@@ -1,7 +1,7 @@
 <?php
 
-require __DIR__ . "/../vendor/autoload.php";
-require __DIR__ . "/../config/app.php";
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../config/app.php";
 
 use App\Core\Logger;
 use App\Models\BankAccount;
